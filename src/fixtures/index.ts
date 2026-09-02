f7484b4c40ebe1437f52c14881e973e346f6895a
Arshdeep singh <arshgill6120@gmail.com>
2026-09-01 21:40:12 -0700

    Add OpenAPI Diff Room source (batch 4 fixtures)

---
+++ src/fixtures/index.ts
@@ -0,0 +1,7 @@
+import v1 from './petstore-v1.yaml?raw'
+import v2 from './petstore-v2.yaml?raw'
+
+export const DEMO_FIXTURE_ID = 'petstore-v1-v2'
+
+export const DEMO_OLD_YAML = v1
+export const DEMO_NEW_YAML = v2
